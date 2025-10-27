19f4cb426531cf9cd33aa228998e13162c9face4
MalinduShasan <malindushasan@gmail.com>
2025-10-27 18:59:18 +0530

    feat(app): integrate Reports component into App layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import './index.css'
+import Reports from './pages/ReportsPage/Reports'
 
 function App() {
   return (
-    <>
+    <div className="min-h-screen bg-gray-50">
+      <Reports/>
       
-    </>
+    </div>
   )
 }
 
