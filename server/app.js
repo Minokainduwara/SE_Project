fbcf09e59653b2d1a70c6429fc6a420a30366ad4
Minokainduwara <minokainduwara@gmail.com>
2025-10-29 17:38:37 +0530

    grocery api tested

--- server/app.js
+++ server/app.js
@@ -1,1 +1,13 @@
-console.log("Server is running on 5000");
+import express from "express";
+import { PORT } from "./config/env.js";
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Grocery System API is Running');
+});    
+
+app.listen(PORT, () =>{
+    console.log(`Grocery App API is running on : http://localhost:${ PORT }`);
+});
+
+export default app;
