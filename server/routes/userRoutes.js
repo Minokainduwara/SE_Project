ecb65bed993765e06dd0448c4d0678839a540824
Minokainduwara <minokainduwara@gmail.com>
2025-10-29 22:42:28 +0530

    routes created

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { registerUser, loginUser, getAllUsers } from "../controllers/userController.js";
+import { protect, adminOnly } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/", protect, adminOnly, getAllUsers); // Admin-only route
+
+export default router;
