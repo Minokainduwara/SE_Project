944569ff08eba12f4ce2491818b8dcf7fd0ae09b
Minokainduwara <minokainduwara@gmail.com>
2025-10-31 02:29:08 +0530

    CORS

--- server/app.js
+++ server/app.js
@@ -5,12 +5,18 @@ import connectDB from "./config/db.js";
 
 dotenv.config();
 
-connectDB();
-
 const app = express();
 
+const ORIGIN = process.env.ORIGIN || "http://localhost:3000";
+
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ORIGIN,
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Example route
@@ -19,19 +25,25 @@ app.get("/", (req, res) => {
 });
 
 // Admin route (example placeholder)
-app.get("/api/admin", (req, res) => {
+app.get("/api/v1/admin", (req, res) => {
   res.json({ message: "Admin route" });
 });
 
 // Seller route (example placeholder)
-app.get("/api/seller", (req, res) => {
+app.get("/api/v1/seller", (req, res) => {
   res.json({ message: "Seller route" });
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
-});
+try {
+  await connectDB();
+  console.log("Database successfully connected");
+  app.listen(PORT, async () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+} catch (error) {
+  console.log(error);
+}
