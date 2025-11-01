70fcd243f6ea813e824d9e107d3c4a84a0241d25
Minokainduwara <minokainduwara@gmail.com>
2025-11-02 02:14:39 +0530

    product routes

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,11 +1,20 @@
 import express from "express";
-import { getProducts, addProduct, updateProduct } from "../controllers/productController.js";
-import { protect, sellerOnly } from "../middleware/authMiddleware.js";
+import {
+  addProduct,
+  getProducts,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/productController.js";
+import { protect, sellerOrAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/", getProducts);
-router.post("/", protect, sellerOnly, addProduct);
-router.put("/:id", protect, sellerOnly, updateProduct);
+router.route("/")
+  .get(getProducts)
+  .post(protect, sellerOrAdmin, addProduct);
+
+router.route("/:id")
+  .put(protect, sellerOrAdmin, updateProduct)
+  .delete(protect, sellerOrAdmin, deleteProduct);
 
 export default router;
