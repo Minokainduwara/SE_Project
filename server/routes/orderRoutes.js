84c11982e2f87945d67f3dcac6eb88e9ed30758d
Minokainduwara <minokainduwara@gmail.com>
2025-11-02 02:55:08 +0530

    orderRoutes

---
+++ server/routes/orderRoutes.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { createOrder, getMyOrders, getAllOrders } from "../controllers/orderController.js";
+import { protect, adminOnly } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", protect, createOrder);         
+router.get("/myorders", protect, getMyOrders);   
+router.get("/", protect, adminOnly, getAllOrders); 
+export default router;
